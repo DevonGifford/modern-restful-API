8090d67f750f04430328698619124a7edc57c6ab
DevonGifford <DevonGifford@outlook.com>
2023-10-19 15:46:51 +0200

    connecting mongoDB and setting up schema - started controllers

---
+++ src/helpers/index.ts
@@ -0,0 +1,9 @@
+import crypto from 'crypto';
+
+const SECRET = 'DEVON-REST-API'; //.env obviously 
+
+export const authentication = (salt: string, password: string): string => {
+  return crypto.createHmac('sha256', [salt, password].join('/')).update(SECRET).digest('hex');
+}
+
+export const random = () => crypto.randomBytes(128).toString('base64');
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 
+
 // import router from './router';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -24,5 +25,10 @@ server.listen(8080, () => {
   console.log('Server running on http://localhost:8080/');
 });
 
-const MONGO_URL = ''; // DB URI
+const MONGO_URL = 'mongodb+srv://devongifford:v27XqD9Moqvp5CgE@cluster0.5mxoa5k.mongodb.net/?retryWrites=true&w=majority'; // DB URI
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+//app.use('/', router());
